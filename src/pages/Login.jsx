0506fd321898aef0cf5ac5558ca45f2caed1429c
vansh jaiswal <vanshjais1777@gmail.com>
2024-09-27 19:13:03 +0530

    last update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Login as loginComponent } from '../components'
+import Loginn from '../components/Login'
 function Login() {
   return (
     <div className='py-8'>
-        <loginComponent />
+      <Loginn />
     </div>
   )
 }
